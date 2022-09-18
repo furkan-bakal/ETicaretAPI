cae30549d9721f6862f5ab8f9f32ca73f9f3dab9
furkanbakal <f.bakal1997@gmail.com>
2022-09-18 22:58:37 +0300

    dosya yükleme endpoint

--- Presentation/ETicaretServer.API/Controllers/ProductController.cs
+++ Presentation/ETicaretServer.API/Controllers/ProductController.cs
@@ -15,10 +15,13 @@ namespace ETicaretServer.API.Controllers
     {
         readonly private IProductReadRepository _productReadRepository;
         readonly private IProductWriteRepository _productWriteRepository;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ProductsController(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository)
+        public ProductsController(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository,
+          IWebHostEnvironment webHostEnvironment)
         {
             _productWriteRepository = productWriteRepository;
+            _webHostEnvironment = webHostEnvironment;
             _productReadRepository = productReadRepository;
         }
 
@@ -86,6 +89,28 @@ namespace ETicaretServer.API.Controllers
         {
             Product product = await _productReadRepository.GetByIdAsync(id, false);
             return Ok(product);
+        }
+        
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Upload()
+        {
+            string uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "resource/product-images");
+
+            if(!Directory.Exists(uploadPath))
+                Directory.CreateDirectory(uploadPath);
+
+            Random r = new();
+
+            foreach (IFormFile file in Request.Form.Files)
+            {
+                string fullPath = Path.Combine(uploadPath, $"{r.Next()}{Path.GetExtension(file.FileName)}");
+
+                using FileStream fileStream = new(fullPath, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, useAsync: false);
+
+                await file.CopyToAsync(fileStream);
+                await fileStream.FlushAsync();
+            }
+            return Ok();
         }
     }
 }
